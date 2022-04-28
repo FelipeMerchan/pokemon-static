16af6dbcbaf7da104e28e915a532c4688225aeb4
Felipe Merchan <felipe-sc@hotmail.es>
2022-04-28 16:56:00 -0500

    Creando una barra de navegación

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 import Head from 'next/head'
 
+import { Navbar } from '../ui'
+
 interface Props {
   children?: React.ReactNode;
   title?: string;
@@ -15,8 +17,10 @@ export const Layout: FC<Props> = ({ children, title }) => {
         <meta name='description' content={`Información sobre el Pokemon ${title}`} />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
       </Head>
-      {/* <Navbar /> */}
-      <main>
+      <Navbar />
+      <main style={{
+        padding: '0 20px'
+      }}>
         {children}
       </main>
     </>
