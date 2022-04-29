9dc6d798a9dd8fe15cfbacd11ab84b55596f3ce3
Felipe Merchan <felipe-sc@hotmail.es>
2022-04-29 16:30:14 -0500

    Tarea - Navegación y página de favoritos

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
-import { Spacer, Text, useTheme } from '@nextui-org/react'
+import NextLink from 'next/link'
+import { Link, Spacer, Text, useTheme } from '@nextui-org/react'
 
 export const Navbar = () => {
   const { theme } = useTheme()
@@ -11,19 +12,30 @@ export const Navbar = () => {
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'flex-start',
-      padding: 'o 20px',
+      padding: '0 20px 0 5px',
       backgroundColor: theme?.colors.gray900.value
     }}>
-      <Image
-        src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
-        alt='Ícono de la app'
-        width={70}
-        height={70}
-      />
-      <Text color='white' h2>P</Text>
-      <Text color='white' h3>okémon</Text>
+      <NextLink href='/' passHref>
+        <Link style={{
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          <Image
+            src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
+            alt='Ícono de la app'
+            width={70}
+            height={70}
+          />
+          <Text color='white' h2>P</Text>
+          <Text color='white' h3>okémon</Text>
+        </Link>
+      </NextLink>
       <Spacer css={{ flex: 1 }}/>
-      <Text color='white'>Favoritos</Text>
+      <NextLink href='/favorites' passHref>
+        <Link>
+          <Text color='white'>Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
   )
 }
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next"
+import { Layout } from "../../components/layouts"
+
+const FavoritesPage: NextPage = () => {
+  return (
+    <Layout title='Pokémon - Favoritos'>
+      <h1>Favorites</h1>
+    </Layout>
+  )
+}
+
+export default FavoritesPage
